4c0d1a962ae65b6cd3d28368723b9d12067146d4
Mario Gamal <mnous@justclean.com>
2020-10-08 11:29:18 +0300

    Add ability to use intercepters and timeout configrations

--- networking/src/main/java/com/justclean/networking/NetworkController.kt
+++ networking/src/main/java/com/justclean/networking/NetworkController.kt
@@ -1,19 +1,28 @@
 package com.justclean.networking
 
+import com.google.gson.JsonElement
 import com.justclean.networking.RequestType.*
-import org.koin.core.KoinComponent
-import org.koin.core.inject
+import io.reactivex.Flowable
+import okhttp3.Interceptor
 
-object NetworkController: KoinComponent {
+object NetworkController {
 
-    private val request: APIService by inject()
+    private lateinit var request: APIService
 
-    fun processRequest(type: RequestType, fullUrl: String) =
-        when (type) {
+    fun init(interceptors: List<Interceptor> = listOf(), debugTimeOut: Long = 30, releaseTimeOut: Long = 10) {
+        request = ServiceBuilder.buildService(interceptors, debugTimeOut, releaseTimeOut)
+    }
+
+    fun processRequest(type: RequestType, fullUrl: String): Flowable<JsonElement> {
+        if (!::request.isInitialized)
+            throw NullPointerException("Please call NetworkController.init() in your Application class")
+
+        return when (type) {
             is GET -> request.getRequest(fullUrl, type.queries)
             is POST -> request.postRequest(fullUrl, type.body)
             is PUT -> request.putRequest(fullUrl, type.body)
             is PATCH -> request.patchRequest(fullUrl, type.body)
             is DELETE -> request.deleteRequest(fullUrl)
         }
+    }
 }
