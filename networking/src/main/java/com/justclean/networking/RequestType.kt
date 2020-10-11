5d6125f9c483606f8d4abb2d0d6c72d2a429acd0
Mario Gamal <mnous@justclean.com>
2020-10-11 12:48:07 +0300

    Make Patch request body optional

--- networking/src/main/java/com/justclean/networking/RequestType.kt
+++ networking/src/main/java/com/justclean/networking/RequestType.kt
@@ -4,6 +4,6 @@ sealed class RequestType {
     data class GET(val queries: HashMap<String, Any> = HashMap()) : RequestType()
     data class POST(val body: RequestBody) : RequestType()
     data class PUT(val body: RequestBody) : RequestType()
-    data class PATCH(val body: RequestBody) : RequestType()
+    data class PATCH(val body: RequestBody?) : RequestType()
     object DELETE : RequestType()
 }
