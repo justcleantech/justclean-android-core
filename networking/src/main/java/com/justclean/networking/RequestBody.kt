93efe4751f16746e9a62efd22d05d136f952df74
Mario Gamal <mnous@justclean.com>
2020-10-19 14:09:50 +0300

    Add EmptyBody() class and publish alpha08

--- networking/src/main/java/com/justclean/networking/RequestBody.kt
+++ networking/src/main/java/com/justclean/networking/RequestBody.kt
@@ -1,3 +1,5 @@
 package com.justclean.networking
 
 interface RequestBody
+
+class EmptyBody: RequestBody
--- networking/src/main/java/com/justclean/networking/RequestType.kt
+++ networking/src/main/java/com/justclean/networking/RequestType.kt
@@ -2,8 +2,8 @@ package com.justclean.networking
 
 sealed class RequestType {
     data class GET(val queries: HashMap<String, Any> = HashMap()) : RequestType()
-    data class POST(val body: RequestBody = object : RequestBody{}) : RequestType()
-    data class PUT(val body: RequestBody = object : RequestBody{}) : RequestType()
-    data class PATCH(val body: RequestBody = object : RequestBody{}) : RequestType()
+    data class POST(val body: RequestBody = EmptyBody()) : RequestType()
+    data class PUT(val body: RequestBody = EmptyBody()) : RequestType()
+    data class PATCH(val body: RequestBody = EmptyBody()) : RequestType()
     object DELETE : RequestType()
 }
