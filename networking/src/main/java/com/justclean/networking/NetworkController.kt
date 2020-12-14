3f6141355adfe25220e47133819da19f7bc9ba43
Mario Gamal <mnous@justclean.com>
2020-12-14 18:04:00 +0300

    Enhance networking serialization

--- networking/src/main/java/com/justclean/networking/NetworkController.kt
+++ networking/src/main/java/com/justclean/networking/NetworkController.kt
@@ -1,28 +1,38 @@
 package com.justclean.networking
 
+import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.google.gson.reflect.TypeToken
 import com.justclean.networking.RequestType.*
 import io.reactivex.Flowable
+import io.reactivex.functions.Function
 import okhttp3.Interceptor
 
 object NetworkController {
 
-    private lateinit var request: APIService
+    var request: APIService? = null
 
     fun init(interceptors: List<Interceptor> = listOf(), debugTimeOut: Long = 30, releaseTimeOut: Long = 10) {
         request = ServiceBuilder.buildService(interceptors, debugTimeOut, releaseTimeOut)
     }
 
-    fun processRequest(type: RequestType, fullUrl: String): Flowable<JsonElement> {
-        if (!::request.isInitialized)
+    inline fun <reified T> processRequest(type: RequestType, fullUrl: String): Flowable<T> {
+        if (request == null)
             throw NullPointerException("Please call NetworkController.init() in your Application class")
 
-        return when (type) {
-            is GET -> request.getRequest(fullUrl, type.queries)
-            is POST -> request.postRequest(fullUrl, type.body)
-            is PUT -> request.putRequest(fullUrl, type.body)
-            is PATCH -> request.patchRequest(fullUrl, type.body)
-            is DELETE -> request.deleteRequest(fullUrl)
+        val response = when (type) {
+            is GET -> request!!.getRequest(fullUrl, type.queries)
+            is POST -> request!!.postRequest(fullUrl, type.body)
+            is PUT -> request!!.putRequest(fullUrl, type.body)
+            is PATCH -> request!!.patchRequest(fullUrl, type.body)
+            is DELETE -> request!!.deleteRequest(fullUrl)
         }
+
+        return response.map(object : Function<JsonElement, T> {
+            override fun apply(t: JsonElement): T {
+                return Gson().fromJson(t, object: TypeToken<T>(){}.type)
+            }
+        })
     }
+
 }
