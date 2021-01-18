b86f2675d9144ebe56b056c2a71446df1bf3a60d
Mario Gamal <mnous@justclean.com>
2021-01-18 12:00:33 +0300

    Fix loading color with enabled states

---
+++ app/src/main/java/com/justclean/core/ui/UIDemoActivity.kt
@@ -0,0 +1,25 @@
+package com.justclean.core.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.justclean.core.R
+import com.justclean.core.heplers.gone
+import com.justclean.core.heplers.visible
+import kotlinx.android.synthetic.main.activity_button.*
+import kotlinx.android.synthetic.main.activity_button.jcBtn
+import kotlinx.android.synthetic.main.activity_uidemo.*
+
+class UIDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uidemo)
+        jcBtn.setOnClickListener {
+            resetBtn.visible()
+        }
+
+        resetBtn.setOnClickListener {
+            jcBtn.reset()
+            it.gone()
+        }
+    }
+}
