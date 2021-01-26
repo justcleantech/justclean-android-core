79963d587c3bf1158d5ff8571916b9303bf9cb1e
Mario Gamal <mnous@justclean.com>
2021-01-26 10:48:41 +0300

    Add binding adapter for the uikit and sample usage for it

--- app/src/main/java/com/justclean/core/ui/UIDemoActivity.kt
+++ app/src/main/java/com/justclean/core/ui/UIDemoActivity.kt
@@ -2,9 +2,12 @@ package com.justclean.core.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.justclean.core.R
+import com.justclean.core.databinding.ActivityUidemoBinding
 import com.justclean.core.heplers.gone
 import com.justclean.core.heplers.visible
+import com.justclean.uikit.validation.ValidationObserver
 import kotlinx.android.synthetic.main.activity_button.*
 import kotlinx.android.synthetic.main.activity_button.jcBtn
 import kotlinx.android.synthetic.main.activity_uidemo.*
@@ -12,7 +15,11 @@ import kotlinx.android.synthetic.main.activity_uidemo.*
 class UIDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_uidemo)
+
+        val binding: ActivityUidemoBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_uidemo)
+        binding.observer = ValidationObserver()
+
         jcBtn.setOnClickListener {
             resetBtn.visible()
         }
@@ -22,12 +29,5 @@ class UIDemoActivity : AppCompatActivity() {
             it.gone()
         }
 
-        enable.setOnClickListener {
-            jcBtn.setButtonEnabled(true)
-        }
-
-        disable.setOnClickListener {
-            jcBtn.setButtonEnabled(false)
-        }
     }
 }
